bfb5ff4f4edf2539f0c07eb113b7b6dcc2317a08
Yonatan Laurence <yangning@bu.edu>
2018-11-22 00:33:24 -0500

    [#1] Replace 'react-mapbox-gl' library with FullStackReact's 'google-maps-react'

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,9 +1,15 @@
 import React, { Component } from 'react'
-import ReactMapGL, { Marker } from 'react-map-gl';
+import { Map, Marker, InfoWindow, GoogleApiWrapper } from 'google-maps-react';
 import { FaStreetView } from 'react-icons/fa';
 import env from '../../env'
 
 const styles = {
+  map: {
+    width: '100%',
+    height: '100%',
+    minHeight: '400px',
+    position: 'relative'
+  },
   marker: {
     backgroundColor: 'transparent',
     color: 'black',
@@ -12,37 +18,82 @@ const styles = {
   }
 }
 
-class Map extends Component {
+class MapContainer extends Component {
   constructor(props) {
     super(props)
 
     this.state = {
-      viewport: {
-        width: '100%',
-        height: '100%',
-        zoom: 15,
-        latitude: props.user.location.latitude,
-        longitude: props.user.location.longitude
-      }
-    }
+      showingInfoWindow: false,
+      activeMarker: {},
+      selectedPlace: {},
+    };
+
   }
+
+  onMarkerClick = (props, marker, e) => {
+    console.log(props)
+    this.setState({
+      selectedPlace: props,
+      activeMarker: marker,
+      showingInfoWindow: true
+    });
+  }
+
+  onMapClicked = (props) => {
+    if (this.state.showingInfoWindow) {
+      this.setState({
+        showingInfoWindow: false,
+        activeMarker: null
+      })
+    }
+  };
+
   render() {
-    const { firstName, location: { latitude, longitude } } = this.props.user
+    const { firstName, location: { address, latitude, longitude } } = this.props.user
     return (
-      <ReactMapGL
-        {...this.state.viewport}
-        mapboxApiAccessToken={env.REACT_APP_MAPBOX_API_KEY_PUBLIC}
-        onViewportChange={(viewport) => this.setState({viewport})}
+      <Map
+        containerStyle={styles.map}
+        google={this.props.google}
+        zoom={15}
+        initialCenter={{
+          lat: latitude,
+          lng: longitude
+        }}
       >
-        <Marker latitude={latitude} longitude={longitude} offsetLeft={-20} offsetTop={-10}>
-          <div style={styles.marker}>
-            <h5><FaStreetView /></h5>
-            <h5 style={{backgroundColor: 'white'}}>{firstName}</h5>
-          </div>
-        </Marker>
-      </ReactMapGL>
+        <Marker
+          name={firstName}
+          address={address}
+          position={{lat: latitude, lng: longitude}}
+          onClick={this.onMarkerClick}
+          style={{color: 'yellow'}}
+        />
+        <InfoWindow
+          marker={this.state.activeMarker}
+          visible={this.state.showingInfoWindow}>
+            <>
+              <h5><FaStreetView /> {this.state.selectedPlace.name}</h5>
+              <p>{this.state.selectedPlace.address}</p>
+            </>
+        </InfoWindow>
+      </Map>
     )
   }
 }
 
-export default Map;
+/*
+<Marker latitude={latitude} longitude={longitude} offsetLeft={-20} offsetTop={-10}>
+  <div style={styles.marker}>
+    <h5><FaStreetView /></h5>
+    <h5 style={{backgroundColor: 'white'}}>{firstName}</h5>
+  </div>
+</Marker>
+*/
+
+const LoadingContainer = (props) => (
+  <div>Loading map...</div>
+)
+
+export default GoogleApiWrapper({
+  apiKey: env.REACT_APP_GOOGLE_MAPS_API_KEY,
+  LoadingContainer: LoadingContainer
+})(MapContainer)
